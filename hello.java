1a0b11f07b392d946062034874aa45b979f201b2
gauriupadhyay11 <gauriupadhyay132@gmail.com>
2025-08-27 15:15:45 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,45 @@
+// public class hello{
+//     public static void main(String[]args) {
+//         int a=2;
+//         int b=5;
+//         int c=a+b;
+//         System.out.println(c);
+
+// }
+// }
+
+
+// public class hello {
+//     public static void main(String[]args) {
+//         System.out.println("hello!gauri upadhyay");
+//     }
+// }
+
+// public class hello {
+//     public static void main(String[] args) {
+//         String name = "Sam";
+//         int rollNumber = 1;
+//         double percentMarks = 99.99;
+//         char result = 'P';
+//         System.out.println("Displaying Result:\n" +name+ " with Roll Number " +
+// rollNumber+ " has Scored " +percentMarks+ 
+// "% Marks and Result is " +result);
+//     }
+// }
+
+// public class hello {
+
+// public class first {
+// 	public static void main(String[] args) {                	      
+// 		int n;
+// 		Scanner sc = new Scanner(System.in);
+// 		n = sc.nextInt();
+// 		int fact =0;
+// 		for(int i=1;i<n;i++) {
+// 			if(n%i==0)
+// 				fact++;
+// 			}
+// 		System.out.println(fact);
+// 		}
+//     }
+// }
